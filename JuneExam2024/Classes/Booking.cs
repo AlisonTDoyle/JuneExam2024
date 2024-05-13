99f207a9fc58cbc4b4508c8f11d831b3c111e4ae
Alison Doyle <S00233102@atu.ie>
2024-05-13 13:03:45 +0100

    Created booking and customer classes

---
+++ JuneExam2024/Classes/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuneExam2024.Classes
+{
+    internal class Booking
+    {
+        //Properties
+        public int BookingID { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfParticipants { get; set; }
+    }
+}
---
+++ JuneExam2024/Classes/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuneExam2024.Classes
+{
+    internal class Customer
+    {
+        //Properties
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
